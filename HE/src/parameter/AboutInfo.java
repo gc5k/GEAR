5d8034ad9aa17eef1f48f10409d2f43f1cdbb14b
zzxiang1985 <zzxiang1985@de986a03-0502-4b20-8e92-0c2ce493ce1b>
2013-03-18 18:01:21 +0000

    Delete HE/src/parameter/AboutInfo.java and add it to the ignore list.

--- HE/src/parameter/AboutInfo.java
+++
@@ -1,6 +0,0 @@
-package parameter;
-
-public class AboutInfo {
-    public static final String WELCOME_MESSAGE = "\n******************************************************************\n| GEAR [GEnetic Analysis Repository] version 0.7.7               |\n| (C) 2013 Guo-Bo Chen, Zhi-Xiang Zhu                            |\n| SVN r262M, built on Mon Mar 18 23:30:14 EST 2013               |\n| GNU General Public License, v2                                 |\n******************************************************************\n";
-    public static final String SVN_REVISION = "262M";
-}
