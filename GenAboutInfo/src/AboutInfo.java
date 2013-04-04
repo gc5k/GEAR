939a1bdd48a0047a277a51f382511735ef339f37
zzxiang1985 <zzxiang1985@de986a03-0502-4b20-8e92-0c2ce493ce1b>
2013-04-04 04:37:30 +0000

    [GenAboutInfo] Delete the unnecessary AboutInfo.java.

--- GenAboutInfo/src/AboutInfo.java
+++
@@ -1,5 +0,0 @@
-
-public class AboutInfo {
-    public static final String WELCOME_MESSAGE = "\n******************************************************************\n| GEAR [GEnetic Analysis Repository] version 0.7.7               |\n| (C) 2013 Guo-Bo Chen, Zhi-Xiang Zhu                            |\n| SVN rexported, built on Mon Mar 18 00:45:34 CST 2013      |\n| GNU General Public License, v2                                 |\n******************************************************************\n";
-    public static final String SVN_REVISION = "exported";
-}
