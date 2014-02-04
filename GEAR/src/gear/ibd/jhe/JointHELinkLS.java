e3e3bfd8632588a748b50c945469cd127e774289
gc5k <gc5k@de986a03-0502-4b20-8e92-0c2ce493ce1b>
2014-02-04 16:34:36 +0000

    [TASK] Moved ParentIBD.java to /GEAR/src/gear/ibd/ibd/ [TASK] Created jhe package, which contains JointHELinkLS.java and JointHELinkML.java.

---
+++ GEAR/src/gear/ibd/jhe/JointHELinkLS.java
@@ -0,0 +1,13 @@
+package gear.ibd.jhe;
+
+public class JointHELinkLS
+{
+	private String ibdFile;
+	private String pheFile;
+	
+	public JointHELinkLS ()
+	{
+		
+	}
+
+}
---
+++ GEAR/src/gear/ibd/jhe/JointHELinkML.java
@@ -0,0 +1,6 @@
+package gear.ibd.jhe;
+
+public class JointHELinkML
+{
+
+}
