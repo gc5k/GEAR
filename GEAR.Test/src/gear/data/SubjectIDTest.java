083361b28cf17de424ca7beee208c280d2174b79
zzxiang1985 <zzxiang1985@de986a03-0502-4b20-8e92-0c2ce493ce1b>
2014-02-12 04:31:14 +0000

    [MISC][JointHELinkLS] If the SubjectIDs do not match, print them to let the user knows and quit.

--- GEAR.Test/src/gear/data/SubjectIDTest.java
+++ GEAR.Test/src/gear/data/SubjectIDTest.java
@@ -11,9 +11,11 @@ public class SubjectIDTest
 	public void test()
 	{
 		SubjectID subject0 = new SubjectID("FAM1", "IND1");
-		assertEquals(subject0, new SubjectID("FAM1", "IND1"));
+		SubjectID subject1 = new SubjectID("FAM1", "IND1");
+		assertEquals(subject0, subject1);
 		assertEquals("FAM1", subject0.getFamilyID());
 		assertEquals("IND1", subject0.getIndividualID());
+		assertNotSame(subject0, subject1);
 	}
 
 }
