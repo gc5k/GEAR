7769f627074253dfb09cda918f357d9ecf683e71
Guo-Bo Chen <chenguobo@gmail.com>
2014-03-07 04:36:42 +0000

    [TASK] Let's make qpca together.

---
+++ GEAR/src/gear/subcommands/qpca/QPCACommand.java
@@ -0,0 +1,50 @@
+package gear.subcommands.qpca;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+
+import gear.subcommands.Command;
+import gear.subcommands.CommandArgumentException;
+import gear.subcommands.CommandArguments;
+import gear.subcommands.CommandImpl;
+
+public class QPCACommand extends Command
+{
+
+	@Override
+	public String getName()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void prepareOptions(Options options)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public CommandArguments parse(CommandLine cmdLine)
+			throws CommandArgumentException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected CommandImpl createCommandImpl()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ GEAR/src/gear/subcommands/qpca/QPCACommandImpl.java
@@ -0,0 +1,16 @@
+package gear.subcommands.qpca;
+
+import gear.subcommands.CommandArguments;
+import gear.subcommands.CommandImpl;
+
+public class QPCACommandImpl extends CommandImpl
+{
+
+	@Override
+	public void execute(CommandArguments cmdArgs)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
