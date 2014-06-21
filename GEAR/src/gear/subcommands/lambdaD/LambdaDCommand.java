dbcee819e8314c3b90e6e73ab5a22655098dbffb
Guo-Bo Chen <chenguobo@gmail.com>
2014-06-21 13:53:27 +0000

    [TASK] Update LambdaD Removed --meta1 --meta1, and replaced them with --mate [args]; Added --meta-gz [a list of files], and --meta-gz-batch [a list of meta files in gz format]; Added --meta-batch [a file contains each line a meta file], and --meta-gz-batch [a file contains each line a meta file in gz format].
    Added --cc-batch [a file each line contains the number of cases and controls for each file in meta-batch/meta-gz-batch].
    
    Hidden option:
    --key, in which user can define the keys of the five fields, snp, beta, se, a1, and a2.

--- GEAR/src/gear/subcommands/lambdaD/LambdaDCommand.java
+++ GEAR/src/gear/subcommands/lambdaD/LambdaDCommand.java
@@ -32,10 +32,17 @@ public class LambdaDCommand extends Command
 	@Override
 	public void prepareOptions(Options options)
 	{
-		options.addOption(OptionBuilder.withDescription(OPT_META1_DESC).withLongOpt(OPT_META1_LONG).hasArg().create(OPT_META1));
-		options.addOption(OptionBuilder.withDescription(OPT_META2_DESC).withLongOpt(OPT_META2_LONG).hasArg().create(OPT_META2));
-		options.addOption(OptionBuilder.withDescription(OPT_CC_DESC).hasArgs(4).create(OPT_CC));
-		options.addOption(OptionBuilder.withDescription(OPT_QT_DESC).hasArgs(2).create(OPT_QT));
+		options.addOption(OptionBuilder.withDescription(OPT_META_DESC).withLongOpt(OPT_META_LONG).hasArgs().create(OPT_META));
+		options.addOption(OptionBuilder.withDescription(OPT_META_BATCH_DESC).withLongOpt(OPT_META_BATCH_LONG).hasArg().create(OPT_META_BATCH));
+		options.addOption(OptionBuilder.withDescription(OPT_META_GZ_DESC).withLongOpt(OPT_META_GZ_LONG).hasArgs().create(OPT_META_GZ));
+		options.addOption(OptionBuilder.withDescription(OPT_META_GZ_BATCH_DESC).withLongOpt(OPT_META_GZ_BATCH_LONG).hasArg().create(OPT_META_GZ_BATCH));
+
+		options.addOption(OptionBuilder.withDescription(OPT_CC_DESC).hasArgs().create(OPT_CC));
+		options.addOption(OptionBuilder.withDescription(OPT_CC_BATCH_LONG_DESC).withLongOpt(OPT_CC_BATCH_LONG).hasArg().create());
+		options.addOption(OptionBuilder.withDescription(OPT_QT_DESC).hasArgs().create(OPT_QT));
+		options.addOption(OptionBuilder.withDescription(OPT_QT_BATCH_LONG_DESC).withLongOpt(OPT_QT_BATCH_LONG).hasArg().create());
+		
+		options.addOption(OptionBuilder.withDescription(OPT_KEY_DESC).hasArgs(5).create(OPT_KEY));		
 	}
 
 	@Override
@@ -43,17 +50,69 @@ public class LambdaDCommand extends Command
 	{
 		LambdaDCommandArguments lamD = new LambdaDCommandArguments();
 
-		lamD.setMeta1(cmdLine.getOptionValue(OPT_META1));
-		lamD.setMeta2(cmdLine.getOptionValue(OPT_META2));
-
-		if (cmdLine.hasOption(OPT_QT))
+		//manual text meta files
+		if (cmdLine.hasOption(OPT_META))
 		{
-			lamD.setQT(cmdLine.getOptionValues(OPT_QT));
+			lamD.setMetaFile(cmdLine.getOptionValues(OPT_META));
+			lamD.setGZ(false);
+			if (cmdLine.hasOption(OPT_QT))
+			{
+				lamD.setQT(cmdLine.getOptionValues(OPT_QT));
+			}
+			if (cmdLine.hasOption(OPT_CC))
+			{
+				lamD.setCC(cmdLine.getOptionValues(OPT_CC));
+			}
+		}
+		
+		//batch text meta batch
+		if (cmdLine.hasOption(OPT_META_BATCH))
+		{
+			lamD.setMetaBatch(cmdLine.getOptionValue(OPT_META_BATCH));
+			lamD.setGZ(false);
+			if (cmdLine.hasOption(OPT_QT_BATCH_LONG))
+			{
+				lamD.setQTbatch(cmdLine.getOptionValue(OPT_QT_BATCH_LONG));
+			}
+			if (cmdLine.hasOption(OPT_CC_BATCH_LONG))
+			{
+				lamD.setCCbatch(cmdLine.getOptionValue(OPT_CC_BATCH_LONG));
+			}
 		}
 
-		if (cmdLine.hasOption(OPT_CC))
+		//manual gzip meta files
+		if (cmdLine.hasOption(OPT_META_GZ))
 		{
-			lamD.setCC(cmdLine.getOptionValues(OPT_CC));
+			lamD.setMetaFile(cmdLine.getOptionValues(OPT_META_GZ));
+			lamD.setGZ(true);
+			if (cmdLine.hasOption(OPT_QT))
+			{
+				lamD.setQT(cmdLine.getOptionValues(OPT_QT));
+			}
+			if (cmdLine.hasOption(OPT_CC))
+			{
+				lamD.setCC(cmdLine.getOptionValues(OPT_CC));
+			}
+		}
+
+		//batch gzip meta files
+		if (cmdLine.hasOption(OPT_META_GZ_BATCH))
+		{
+			lamD.setMetaBatch(cmdLine.getOptionValue(OPT_META_GZ_BATCH));
+			lamD.setGZ(true);
+			if (cmdLine.hasOption(OPT_QT_BATCH_LONG))
+			{
+				lamD.setQTbatch(cmdLine.getOptionValue(OPT_QT_BATCH_LONG));
+			}
+			if (cmdLine.hasOption(OPT_CC_BATCH_LONG))
+			{
+				lamD.setCCbatch(cmdLine.getOptionValue(OPT_CC_BATCH_LONG));
+			}
+		}
+
+		if (cmdLine.hasOption(OPT_KEY))
+		{
+			lamD.setKey(cmdLine.getOptionValues(OPT_KEY));
 		}
 		return lamD;
 	}
@@ -64,17 +123,36 @@ public class LambdaDCommand extends Command
 		return new LambdaDCommandImpl();
 	}
 
-	private final static String OPT_META1 = "m1";
-	private final static String OPT_META1_LONG = "meta1";
-	private final static String OPT_META1_DESC = "The summary statistics for the first data";
-	private final static String OPT_META2 = "m2";
-	private final static String OPT_META2_LONG = "meta2";
-	private final static String OPT_META2_DESC = "The summary statistics for the second data";
+	private final static String OPT_META = "m";
+	private final static String OPT_META_LONG = "meta";
+	private final static String OPT_META_DESC = "The summary statistic files";
+
+	private final static String OPT_META_GZ = "mg";
+	private final static String OPT_META_GZ_LONG = "meta-gz";
+	private final static String OPT_META_GZ_DESC = "The summary statistic files in gz format";
 
 	private final static String OPT_CC = "cc";
 	private final static String OPT_CC_DESC = "Case-control study: #case 1, #ctrl 1, #case 2, #ctrl 2";
 
 	private final static String OPT_QT = "qt";
 	private final static String OPT_QT_DESC = "Quantitative trait: #sample size 1, #sample size 2";
+
+
+	private final static String OPT_META_BATCH = "mb";
+	private final static String OPT_META_BATCH_LONG = "meta-batch";
+	private final static String OPT_META_BATCH_DESC = "The summary statistic batch";
+
+	private final static String OPT_META_GZ_BATCH = "mgb";
+	private final static String OPT_META_GZ_BATCH_LONG = "meta-gz-batch";
+	private final static String OPT_META_GZ_BATCH_DESC = "The summary statistic files in gz format";
+
+	private final static String OPT_CC_BATCH_LONG = "cc-batch";
+	private final static String OPT_CC_BATCH_LONG_DESC = "Case-control study: #case 1, #ctrl 1, #case 2, #ctrl 2";
+
+	private final static String OPT_QT_BATCH_LONG = "qt-batch";
+	private final static String OPT_QT_BATCH_LONG_DESC = "Quantitative trait: #sample size 1, #sample size 2";
+
+	private final static String OPT_KEY = "key";
+	private final static String OPT_KEY_DESC = "Self defined key workds: snp, beta, se, a1, a2";
 
 }
