120aa03d7b0e0f6a342b194aca47393a0f19506b
Guo-Bo Chen <chenguobo@gmail.com>
2014-10-18 10:05:14 +0000

    [TASK] Removed rows and columns which do not have gwas summary results.

--- GEAR/src/gear/subcommands/lambdaD/LambdaDCommandImpl.java
+++ GEAR/src/gear/subcommands/lambdaD/LambdaDCommandImpl.java
@@ -13,6 +13,9 @@ import java.util.Set;
 
 import org.apache.commons.math.MathException;
 import org.apache.commons.math.distribution.ChiSquaredDistributionImpl;
+import org.apache.commons.math.linear.Array2DRowRealMatrix;
+import org.apache.commons.math.linear.LUDecompositionImpl;
+import org.apache.commons.math.linear.RealMatrix;
 import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
 import org.apache.commons.math.stat.ranking.NaNStrategy;
 import org.apache.commons.math.stat.ranking.NaturalRanking;
@@ -81,12 +84,7 @@ public class LambdaDCommandImpl extends CommandImpl
 		Logger.printUserLog("Results has been saved in '" + lamArgs.getOutRoot() + ".lmat'.");
 		
 //run mate-analysis
-		Set<String> keys = MetaSNPTable.keySet();
-		for(Iterator<String> e=keys.iterator();e.hasNext();)
-		{
-			String key = e.next();
-			ArrayList<Integer> Int = MetaSNPTable.get(key);
-		}
+		MetaAnalysis();
 	}
 
 	private void initial()
@@ -139,6 +137,153 @@ public class LambdaDCommandImpl extends CommandImpl
 			HashMap<String, MetaStat> m = readMeta(i);
 			meta.add(m);
 		}
+	}
+
+	private void MetaAnalysis()
+	{
+		double[][] Mx = new double[MetaFile.length][MetaFile.length];
+		for (int i = 0; i < Mx.length; i++)
+		{
+			for (int j = 0; j < i + 1; j++)
+			{
+				Mx[i][j] = zMat[i][j];
+				Mx[j][i] = zMat[i][j];
+			}
+		}
+
+		RealMatrix gg = new Array2DRowRealMatrix(Mx);
+		RealMatrix gg_Inv = (new LUDecompositionImpl(gg)).getSolver().getInverse();
+//		InvMx = gg_Inv.getData();
+		RealMatrix Unit = new Array2DRowRealMatrix(MetaFile.length, 1);
+		for (int i = 0; i < Unit.getRowDimension(); i++)
+		{
+			Unit.setEntry(i, 0, 1);
+		}
+		RealMatrix tmp = Unit.transpose().multiply(gg_Inv);
+		RealMatrix tmp1 = tmp.multiply(Unit);
+		RealMatrix W = tmp.scalarMultiply(1/tmp1.getEntry(0, 0));
+
+		Set<String> keys = MetaSNPTable.keySet();
+		for (Iterator<String> e=keys.iterator(); e.hasNext();)
+		{
+			String key = e.next();
+			ArrayList<Integer> Int = MetaSNPTable.get(key);
+			//common snp only
+			if(Int.get(Int.size()-1) == MetaFile.length)
+			{
+				MetaCommon(key, Mx, W, Int);
+			}
+			else
+			{
+				MetaSNP(key, Mx, Int);
+			}
+		}
+	}
+
+	private void MetaSNP(String key, double[][] Mx, ArrayList<Integer> Int)
+	{
+		int[] idx = new int[Int.get(Int.size()-1)];
+		
+		int cnt = 0;
+		for(int i = 0; i < Int.size() - 1; i++)
+		{
+			if(Int.get(i) == 0) continue;
+			idx[cnt++] = i;
+		}
+
+		double[][] mx = new double[idx.length][idx.length];
+		for(int i = 0; i < idx.length; i++)
+		{
+			for(int j = 0; j < idx.length; j++)
+			{
+				mx[i][j] = Mx[idx[i]][idx[j]];
+			}
+		}
+
+		RealMatrix gg = new Array2DRowRealMatrix(mx);
+		RealMatrix gg_Inv = (new LUDecompositionImpl(gg)).getSolver().getInverse();
+//		double[][] Invmx = gg_Inv.getData();
+		RealMatrix Unit = new Array2DRowRealMatrix(mx.length, 1);
+		for (int i = 0; i < Unit.getRowDimension(); i++)
+		{
+			Unit.setEntry(i, 0, 1);
+		}
+		RealMatrix tmp = Unit.transpose().multiply(gg_Inv);
+		RealMatrix tmp1 = tmp.multiply(Unit);
+		RealMatrix W = tmp.scalarMultiply(1/tmp1.getEntry(0, 0));
+
+		double gb = 0;
+		double gse = 0;
+		double[] se = new double[W.getColumnDimension()];
+		
+		HashMap<String, MetaStat> m1 = meta.get(idx[0]);
+		MetaStat ms1 = m1.get(key);
+		gb += ms1.getEffect() * W.getEntry(0, 0);
+		se[0] = ms1.getSE();
+
+		for (int i = 1; i < idx.length; i++)
+		{
+			HashMap<String, MetaStat> m2 = meta.get(idx[i]);
+			MetaStat ms2 = m2.get(key); 
+			se[i] = ms2.getSE();
+
+			if (ms1.getA1() == ms2.getA1() || ms1.getA1() == SNPMatch.Flip(ms2.getA1())) //match A1 in the second meta
+			{
+				gb += ms2.getEffect() * W.getEntry(0, i);
+			}
+			else if (ms1.getA1() == ms2.getA2() || ms1.getA1() == SNPMatch.Flip(ms2.getA2())) //match A2 in the second meta
+			{
+				gb += (-1) * ms2.getEffect() * W.getEntry(0, i);
+			}
+		}
+
+		for (int i = 0; i < W.getRowDimension(); i++)
+		{
+			for (int j = 0; j < W.getRowDimension(); j++)
+			{
+				gse += W.getEntry(0, i) * W.getEntry(0, j) * se[i] * se[j] * Mx[i][j];
+			}
+		}
+		gse = Math.sqrt(gse);
+		System.out.println(key + " " +gb + " " + gse + " " + Int.toString());
+	}
+
+	private void MetaCommon(String key, double[][] Mx, RealMatrix W, ArrayList<Integer> Int)
+	{
+		double gb = 0;
+		double gse = 0;
+		double[] se = new double[W.getColumnDimension()];
+		
+		HashMap<String, MetaStat> m1 = meta.get(0);
+		MetaStat ms1 = m1.get(key);
+		gb += ms1.getEffect() * W.getEntry(0, 0);
+		se[0] = ms1.getSE();
+			
+		for(int i = 1; i < meta.size(); i++)
+		{
+			HashMap<String, MetaStat> m2 = meta.get(i);
+			MetaStat ms2 = m2.get(key); 
+			se[i] = ms2.getSE();
+
+			if (ms1.getA1() == ms2.getA1() || ms1.getA1() == SNPMatch.Flip(ms2.getA1())) //match A1 in the second meta
+			{
+				gb += ms2.getEffect() * W.getEntry(0, i);
+			}
+			else if (ms1.getA1() == ms2.getA2() || ms1.getA1() == SNPMatch.Flip(ms2.getA2())) //match A2 in the second meta
+			{
+				gb += (-1) * ms2.getEffect() * W.getEntry(0, i);
+			}
+		}
+
+		for(int i = 0; i < W.getRowDimension(); i++)
+		{
+			for(int j = 0; j < W.getRowDimension(); j++)
+			{
+				gse += W.getEntry(0, i) * W.getEntry(0, j) * se[i] * se[j] * Mx[i][j];
+			}
+		}
+		gse = Math.sqrt(gse);
+		System.out.println(key + " " +gb + " " + gse + " " + Int.toString());
 	}
 
 	private HashMap<String, MetaStat> readMeta(int metaIdx)
@@ -857,4 +1002,6 @@ public class LambdaDCommandImpl extends CommandImpl
 	private double[][] olCtrlMat;
 	private double[][] olCsMat;
 	private double[][] kMat;
+	
+//	private double[][] InvMx;
 }
