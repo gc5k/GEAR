b73efa7af07627c482e5b2bca9a34c488823ff78
Guo-Bo Chen <chenguobo@gmail.com>
2014-11-11 22:30:45 +0000

    [REFACTOR] Using "svnversion -n". It does not depend on physical location of svnversion.

--- GenAboutInfo/src/AboutInfo.java
+++ GenAboutInfo/src/AboutInfo.java
@@ -1,5 +1,5 @@
 
 public class AboutInfo {
-    public static final String WELCOME_MESSAGE = "\n******************************************************************\n| GEAR [GEnetic Analysis Repository] version 0.7.7               |\n| (C) 2013 Guo-Bo Chen, Zhi-Xiang Zhu                            |\n| SVN r667M, built on Tue Nov 11 22:02:15 EST 2014               |\n| GNU General Public License, v2                                 |\n******************************************************************\n";
-    public static final String SVN_REVISION = "667M";
+    public static final String WELCOME_MESSAGE = "\n******************************************************************\n| GEAR [GEnetic Analysis Repository] version 0.7.7               |\n| (C) 2013 Guo-Bo Chen, Zhi-Xiang Zhu                            |\n| SVN r668M, built on Wed Nov 12 08:27:42 EST 2014               |\n| GNU General Public License, v2                                 |\n******************************************************************\n";
+    public static final String SVN_REVISION = "668M";
 }
