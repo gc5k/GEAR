7638a18ae3f08b379fd751916c61b939dffce685
gc5k <gc5k@de986a03-0502-4b20-8e92-0c2ce493ce1b>
2014-11-11 12:09:22 +0000

    [REFACTOR] After rounds of back and forth, svnversion works in java. The major reason is that there were three, different versions, svnversions installed on Mac. Now, only the svnversion 1.8 has been retained, and the other two (1.6, and 1.7) have been deleted.

--- GenAboutInfo/src/AboutInfo.java
+++ GenAboutInfo/src/AboutInfo.java
@@ -1,5 +1,5 @@
 
 public class AboutInfo {
-    public static final String WELCOME_MESSAGE = "\n******************************************************************\n| GEAR [GEnetic Analysis Repository] version 0.7.7               |\n| (C) 2013 Guo-Bo Chen, Zhi-Xiang Zhu                            |\n| SVN r629:630M, built on Tue Nov 11 13:20:55 EST 2014           |\n| GNU General Public License, v2                                 |\n******************************************************************\n";
-    public static final String SVN_REVISION = "629:630M";
+    public static final String WELCOME_MESSAGE = "\n******************************************************************\n| GEAR [GEnetic Analysis Repository] version 0.7.7               |\n| (C) 2013 Guo-Bo Chen, Zhi-Xiang Zhu                            |\n| SVN r667M, built on Tue Nov 11 22:02:15 EST 2014               |\n| GNU General Public License, v2                                 |\n******************************************************************\n";
+    public static final String SVN_REVISION = "667M";
 }
