68c6b892ecc6fba2ba200bf028206859323e6996
gc5k <gc5k@de986a03-0502-4b20-8e92-0c2ce493ce1b>
2015-01-25 00:54:39 +0000

    [REFACTOR] Refine rand.

--- GEAR/src/gear/subcommands/randmeta/BetaVec.java
+++ GEAR/src/gear/subcommands/randmeta/BetaVec.java
@@ -1,43 +1,112 @@
 package gear.subcommands.randmeta;
 
+import java.util.ArrayList;
+
+import org.apache.commons.math.MathException;
+import org.apache.commons.math.stat.correlation.PearsonsCorrelation;
+
+import gear.util.Logger;
+import gear.util.NewIt;
+
 public class BetaVec
 {
-	public BetaVec(double b1, double b2, double se1, double se2)
+	public BetaVec()
 	{
-		this.b1 = b1;
-		this.b2 = b2;
-		this.se1 = se1;
-		this.se2 = se2;
+		sum= NewIt.newArrayList();
+	}
+
+	public void addStats(double b1, double b2, double se1, double se2)
+	{
+		ArrayList<Double> s = NewIt.newArrayList();
+		s.add(b1);
+		s.add(b2);
+		s.add(se1);
+		s.add(se2);
+		s.add(b1/se1);
+		s.add(b2/se2);
+		sum.add(s);
+	}
+
+	public void setSelected()
+	{
+		selIdx = new int[sum.size()];
+	}
+	
+	public void setSelected(int[] idx)
+	{
+		selIdx = idx;
+	}
+
+	public void CalCorrelation()
+	{
+		double[][] beta = new double[selIdx.length][2];
+		double[][] zscore = new double[selIdx.length][2];
 		
-		z1 = b1/se1;
-		z2 = b2/se2;
+		for(int i = 0; i < selIdx.length; i++)
+		{
+			ArrayList<Double> s = sum.get(selIdx[i]);
+			beta[i][0] = s.get(0);
+			beta[i][1] = s.get(1);
+			
+			zscore[i][0] = s.get(4);
+			zscore[i][1] = s.get(5);
+		}
+
+		PearsonsCorrelation bpc = new PearsonsCorrelation(beta);
+		PearsonsCorrelation zpc = new PearsonsCorrelation(zscore);
+		
+		rb = bpc.getCorrelationMatrix().getData()[1][0];
+		rz = zpc.getCorrelationMatrix().getData()[1][0];
+		
+		try
+		{
+			prb = bpc.getCorrelationPValues().getData()[1][0];
+			prz = zpc.getCorrelationPValues().getData()[1][0];
+		}
+		catch (MathException e)
+		{
+			e.printStackTrace();
+		}
+		
 	}
 
-	public double getB1()
+	public double getBcorrelation()
 	{
-		return b1;
+		return rb;
 	}
 	
-	public double getB2()
+	public double getZcorrelation()
 	{
-		return b2;
+		return rz;
 	}
 	
-	public double getZ1()
+	public double getPvBcorrelation()
 	{
-		return z1;
+		return prb;
 	}
 	
-	public double getZ2()
+	public double getPvZcorrelation()
 	{
-		return z2;
+		return prz;
 	}
 
-	private double b1;
-	private double se1;
-	private double z1;
+	public void printOut()
+	{
+		Logger.printUserLog("Effective number of markers is: " + selIdx.length);
+		Logger.printUserLog("Genetic effect correlation: " + rb);
+		Logger.printUserLog("p-value for z score (two-tails): " + prb);
+		Logger.printUserLog("z score correlation: " + rz);
+		Logger.printUserLog("p-value for z score (two-tails): " + prz);
+	}
 
-	private double b2;
-	private double se2;
-	private double z2;
+	ArrayList<ArrayList<Double>> sum = null;
+	
+	private int[] selIdx;
+	
+	private double rb;
+	private double prb;
+
+	private double rz;
+	private double prz;
+
 }
