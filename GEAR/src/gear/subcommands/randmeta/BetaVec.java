910bf4751e7c0d58a8bd68dbc77f7a4b54369ba8
gc5k <gc5k@de986a03-0502-4b20-8e92-0c2ce493ce1b>
2015-01-25 11:21:34 +0000

    [BUG] Fixed bugs in printing.

--- GEAR/src/gear/subcommands/randmeta/BetaVec.java
+++ GEAR/src/gear/subcommands/randmeta/BetaVec.java
@@ -25,6 +25,8 @@ public class BetaVec
 		s.add(se2);
 		s.add(b1/se1);
 		s.add(b2/se2);
+		s.add(b1*b1/(se1*se1));
+		s.add(b2*b2/(se2*se2));
 		sum.add(s);
 	}
 
@@ -42,7 +44,8 @@ public class BetaVec
 	{
 		double[][] beta = new double[selIdx.length][2];
 		double[][] zscore = new double[selIdx.length][2];
-		
+		double[][] chiscore = new double[selIdx.length][2];
+
 		for(int i = 0; i < selIdx.length; i++)
 		{
 			ArrayList<Double> s = sum.get(selIdx[i]);
@@ -51,19 +54,24 @@ public class BetaVec
 			
 			zscore[i][0] = s.get(4);
 			zscore[i][1] = s.get(5);
+			
+			chiscore[i][0] = s.get(6);
+			chiscore[i][1] = s.get(7);
 		}
 
-		
 		PearsonsCorrelation bpc = new PearsonsCorrelation(beta);
 		PearsonsCorrelation zpc = new PearsonsCorrelation(zscore);
+		PearsonsCorrelation chipc = new PearsonsCorrelation(chiscore);
 		
 		rb = bpc.getCorrelationMatrix().getData()[1][0];
 		rz = zpc.getCorrelationMatrix().getData()[1][0];
+		rchi = chipc.getCorrelationMatrix().getData()[1][0];
 		
 		try
 		{
 			prb = bpc.getCorrelationPValues().getData()[1][0];
 			prz = zpc.getCorrelationPValues().getData()[1][0];
+			prchi = chipc.getCorrelationPValues().getData()[1][0];
 		}
 		catch (MathException e)
 		{
@@ -77,6 +85,8 @@ public class BetaVec
 		zlm = new SimpleRegression();
 		zlm.addData(zscore);		
 		
+		chilm = new SimpleRegression();
+		chilm.addData(chiscore);
 	}
 
 	public double getBcorrelation()
@@ -107,14 +117,23 @@ public class BetaVec
 
 		Logger.printUserLog("Effects regression:");
 		Logger.printUserLog("Effect regression intercept: " + blm.getIntercept() + " (" + blm.getInterceptStdErr() + ")");
-		Logger.printUserLog("Effect regression intercept: " + blm.getSlope() + " (" + blm.getSlopeStdErr() + ")");
+		Logger.printUserLog("Effect regression coefficient: " + blm.getSlope() + " (" + blm.getSlopeStdErr() + ")");
 
+		
 		Logger.printUserLog("z score correlation: " + rz);
 		Logger.printUserLog("p-value for z score (two-tails): " + prz);
+
 		Logger.printUserLog("Z score regression:");
 		Logger.printUserLog("Effect regression intercept: " + zlm.getIntercept() + " (" + zlm.getInterceptStdErr() + ")");
-		Logger.printUserLog("Effect regression intercept: " + zlm.getSlope() + " (" + zlm.getSlopeStdErr() + ")");
+		Logger.printUserLog("Effect regression coefficient: " + zlm.getSlope() + " (" + zlm.getSlopeStdErr() + ")");
 
+		
+		Logger.printUserLog("Chisq score correlation: " + rchi);
+		Logger.printUserLog("p-value for z score (two-tails): " + prchi);
+
+		Logger.printUserLog("Chisq score regression:");
+		Logger.printUserLog("Effect regression intercept: " + chilm.getIntercept() + " (" + chilm.getInterceptStdErr() + ")");
+		Logger.printUserLog("Effect regression coefficient: " + chilm.getSlope() + " (" + chilm.getSlopeStdErr() + ")");
 	}
 
 	ArrayList<ArrayList<Double>> sum = null;
@@ -127,6 +146,10 @@ public class BetaVec
 	private double rz;
 	private double prz;
 
+	private double rchi;
+	private double prchi;
+
 	private SimpleRegression blm;
 	private SimpleRegression zlm;
+	private SimpleRegression chilm;
 }
