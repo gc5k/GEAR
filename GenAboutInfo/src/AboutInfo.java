e45ab4f4bb519a4beb9a756465f4ced21ce9fa39
gc5k <gc5k@d-i184-36-123.staff.eduroam.uq.edu.au>
2015-08-21 10:51:02 +1000

    GenAbout uses git information

--- GenAboutInfo/src/AboutInfo.java
+++ GenAboutInfo/src/AboutInfo.java
@@ -1,5 +1,5 @@
 
 public class AboutInfo {
-    public static final String WELCOME_MESSAGE = "\n******************************************************************\n| GEAR [GEnetic Analysis Repository] version 0.7.7               |\n| (C) 2013 Guo-Bo Chen, Zhi-Xiang Zhu                            |\n| SVN r668M, built on Wed Nov 12 08:27:42 EST 2014               |\n| GNU General Public License, v2                                 |\n******************************************************************\n";
-    public static final String SVN_REVISION = "668M";
+    public static final String WELCOME_MESSAGE = "\n******************************************************************\n| GEAR [GEnetic Analysis Repository] version 0.7.7               |\n| (C) 2013 Guo-Bo Chen, Zhi-Xiang Zhu                            |\n| Git r753, built on Fri Aug 21 09:40:00 AEST 2015               |\n| GNU General Public License, v2                                 |\n******************************************************************\n";
+    public static final String SVN_REVISION = "753";
 }
--- GenAboutInfo/src/GenAboutInfo.java
+++ GenAboutInfo/src/GenAboutInfo.java
@@ -1,76 +1,64 @@
-
-
 import java.io.*;
 import java.util.Calendar;
 
 public class GenAboutInfo {
-	
-	private static String genWelcomeMessage(String svnRev) {
-		int width = 66;  // No. of characters of each line of the welcome message  
+	private static String genWelcomeMessage(String svnRev) 
+	{
+		int width = 66; // No. of characters of each line of the welcome message
 		String line;
-		
-		String welcomeMessage =
-				"\\n" +
-				"******************************************************************\\n" +
-				"| GEAR [GEnetic Analysis Repository] version 0.7.7               |\\n" +
-				"| (C) 2013 Guo-Bo Chen, Zhi-Xiang Zhu                            |\\n";
-		
-		line =	"| SVN r" + svnRev + ", built on ";
+		String welcomeMessage = "\\n" + "******************************************************************\\n"
+				+ "| GEAR [GEnetic Analysis Repository] version 0.7.7               |\\n"
+				+ "| (C) 2013 Guo-Bo Chen, Zhi-Xiang Zhu                            |\\n";
+		line = "| Git r" + svnRev + ", built on ";
 		line += Calendar.getInstance().getTime();
 		for (int i = width - line.length() - 1; i > 0; --i) {
 			line += " ";
 		}
 		line += "|\\n";
 		welcomeMessage += line;
-		
-		welcomeMessage +=
-				"| GNU General Public License, v2                                 |\\n" +
-				"******************************************************************\\n";
-		
+		welcomeMessage += "| GNU General Public License, v2                                 |\\n"
+				+ "******************************************************************\\n";
 		return welcomeMessage;
 	}
 
 	/**
-	 * @param args	[package-name]
+	 * @param args
+	 *            [package-name]
 	 */
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception 
+	{
 		String packageName = null;
 		if (args.length >= 1) {
 			packageName = args[0];
 		}
-		
 		String packageDeclaration = "", folder = "src/";
 		if (packageName != null) {
 			packageDeclaration = "package " + args[0] + ";\n";
 			folder += packageName.replace('.', '/') + "/";
 		}
-		
 		// Read the SVN revision number
-//		Process proc = Runtime.getRuntime().exec("svnversion -version");
-		
-		Process proc = Runtime.getRuntime().exec("svnversion -n");
-//		Process proc = Runtime.getRuntime().exec("which svnversion");
+		// Process proc = Runtime.getRuntime().exec("svnversion -version");
+		Process proc = Runtime.getRuntime().exec("git rev-list head");
+		// Process proc = Runtime.getRuntime().exec("which svnversion");
 
-//		Process proc = Runtime.getRuntime().exec("date");
+		// Process proc = Runtime.getRuntime().exec("date");
 
 		BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-		String svnRev = reader.readLine();
+		int cnt = 0;
+		while (reader.readLine() != null) 
+		{
+			cnt++;
+		}
 		// Welcome message
+		String svnRev = (new Integer(cnt)).toString();
 		String welcomeMessage = genWelcomeMessage(svnRev);
-		
 		// Write the revision number to a java source file
-		String writeContent =
-				packageDeclaration +
-				"\n" +
-				"public class AboutInfo {\n" +
-				"    public static final String WELCOME_MESSAGE = \"" + welcomeMessage + "\";\n" + 
-				"    public static final String SVN_REVISION = \"" + svnRev + "\";\n" +
-				"}";
+		String writeContent = packageDeclaration + "\n" + "public class AboutInfo {\n"
+				+ "    public static final String WELCOME_MESSAGE = \"" + welcomeMessage + "\";\n"
+				+ "    public static final String SVN_REVISION = \"" + svnRev + "\";\n" + "}";
 		FileWriter writer = new FileWriter(folder + "AboutInfo.java");
 		writer.write(writeContent);
 		writer.close();
-		
-		System.out.println("SVN Revision: " + svnRev);
+		System.out.println("Git Revision: " + svnRev);
 	}
-
 }
