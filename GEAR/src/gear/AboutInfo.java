b18c71e9ff51043fee662f2ac49e1dbfc3c97aea
gc5k <gc5k@192.168.31.213>
2015-12-15 17:40:39 +0800

    Updated version tag, minor

--- GEAR/src/gear/AboutInfo.java
+++ GEAR/src/gear/AboutInfo.java
@@ -1,5 +1,5 @@
 package gear;
 public class AboutInfo {
-    public static final String WELCOME_MESSAGE = "\n******************************************************************\n| GEAR [GEnetic Analysis Repository] version 0.7.7               |\n| (C) 2013 Guo-Bo Chen, Zhi-Xiang Zhu                            |\n| Git r766, built on Thu Dec 14 14:06:18 AEST 2015                |\n| GNU General Public License, v2                                 |\n******************************************************************\n";
-    public static final String SVN_REVISION = "766";
+    public static final String WELCOME_MESSAGE = "\n******************************************************************\n| GEAR [GEnetic Analysis Repository] version 0.7.7               |\n| (C) 2013 Guo-Bo Chen, Zhi-Xiang Zhu                            |\n| Git r767, built on Thu Dec 15 14:06:18 AEST 2015               |\n| GNU General Public License, v2                                 |\n******************************************************************\n";
+    public static final String SVN_REVISION = "767";
 }
