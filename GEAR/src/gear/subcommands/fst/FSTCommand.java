b74b635b91585309bc453df75d57a2c82f24274b
gc5k <chenguobo@gmail.com>
2018-06-07 15:10:42 +0800

    added data management for fst

--- GEAR/src/gear/subcommands/fst/FSTCommand.java
+++ GEAR/src/gear/subcommands/fst/FSTCommand.java
@@ -26,6 +26,32 @@ public class FSTCommand extends Command {
 	public void prepareOptions(Options options) {
 		options.addOption(OptionBuilder.withDescription(OPT_BFILE_DESC).withLongOpt(OPT_BFILE_LONG).hasArg().isRequired().create());
 		options.addOption(OptionBuilder.withDescription(OPT_GROUP_DESC).withLongOpt(OPT_GROUP_LONG).hasArg().isRequired().create());
+		
+		options.addOption(OptionBuilder.withDescription(OPT_KEEP_DESC).withLongOpt(OPT_KEEP_LONG).hasArg().create());
+		options.addOption(
+				OptionBuilder.withDescription(OPT_REMOVE_DESC).withLongOpt(OPT_REMOVE_LONG).hasArg().create());
+		options.addOption(OptionBuilder.withDescription(OPT_KEEP_FAM_DESC).withLongOpt(OPT_KEEP_FAM_LONG).hasArg().create());
+		options.addOption(
+				OptionBuilder.withDescription(OPT_REMOVE_FAM_DESC).withLongOpt(OPT_REMOVE_FAM_LONG).hasArg().create());
+		
+		options.addOption(
+				OptionBuilder.withDescription(OPT_EXTRACT_DESC).withLongOpt(OPT_EXTRACT_LONG).hasArg().create());
+		options.addOption(
+				OptionBuilder.withDescription(OPT_EXCLUDE_DESC).withLongOpt(OPT_EXCLUDE_LONG).hasArg().create());
+
+		options.addOption(OptionBuilder.withDescription(OPT_CHR_DESC).withLongOpt(OPT_CHR_LONG).hasArgs().create());
+		options.addOption(
+				OptionBuilder.withDescription(OPT_NOT_CHR_DESC).withLongOpt(OPT_NOT_CHR_LONG).hasArgs().create());
+
+		options.addOption(OptionBuilder.withDescription(OPT_MAF_DESC).withLongOpt(OPT_MAF_LONG).hasArg().create());
+		options.addOption(
+				OptionBuilder.withDescription(OPT_MAX_MAF_DESC).withLongOpt(OPT_MAX_MAF_LONG).hasArg().create());
+		options.addOption(OptionBuilder.withDescription(OPT_GENO_DESC).withLongOpt(OPT_GENO_LONG).hasArg().create());
+		options.addOption(
+				OptionBuilder.withDescription(OPT_ZERO_VAR_DESC).withLongOpt(OPT_ZERO_VAR_LONG).create());
+		options.addOption(
+				OptionBuilder.withDescription(OPT_MAF_RANGE_DESC).withLongOpt(OPT_MAF_RANGE_LONG).hasArgs().create());
+
 	}
 
 	@Override
@@ -33,6 +59,19 @@ public class FSTCommand extends Command {
 		FSTCommandArguments fstArgs = new FSTCommandArguments();
 
 		parseFileArguments((CommandArguments) fstArgs, cmdLine);
+		parseSampleFilterArguments((CommandArguments) fstArgs, cmdLine);
+		parseFamilyFilterArguments((CommandArguments) fstArgs, cmdLine);
+
+		parseSNPFilterFileArguments((CommandArguments) fstArgs, cmdLine);
+		parseSNPFilterChromosomeArguments((CommandArguments) fstArgs, cmdLine);
+
+		parseMAFArguments((CommandArguments) fstArgs, cmdLine);
+		parseMAXMAFArguments((CommandArguments) fstArgs, cmdLine);
+		parseGENOArguments((CommandArguments) fstArgs, cmdLine);
+		parseZeroVarArguments((CommandArguments) fstArgs, cmdLine);
+
+		parseMAFRangeArguments((CommandArguments) fstArgs, cmdLine);
+		
 		fstArgs.setGroup(cmdLine.getOptionValue(OPT_GROUP_LONG));
 		return fstArgs;
 	}
--- GEAR/src/gear/subcommands/fst/FSTCommandImpl.java
+++ GEAR/src/gear/subcommands/fst/FSTCommandImpl.java
@@ -1,15 +1,25 @@
 package gear.subcommands.fst;
 
+import gear.family.GenoMatrix.GenotypeMatrix;
+import gear.family.plink.PLINKParser;
+import gear.family.qc.rowqc.SampleFilter;
 import gear.subcommands.CommandArguments;
 import gear.subcommands.CommandImpl;
+import gear.util.Logger;
 
 public class FSTCommandImpl extends CommandImpl {
 
+	private GenotypeMatrix pGM;
 	private FSTCommandArguments fstArgs;
 	@Override
 	public void execute(CommandArguments cmdArgs) {
 		fstArgs = (FSTCommandArguments) cmdArgs;
-		System.out.println(fstArgs.getGroupFile());
+
+		PLINKParser pp = PLINKParser.parse(fstArgs);
+		SampleFilter sf = new SampleFilter(pp.getPedigreeData(), cmdArgs);
+		pGM = new GenotypeMatrix(sf.getSample(), pp.getMapData(), cmdArgs);
+		
+		Logger.printUserLog("Estimating Fst for " + fstArgs.getBFile());
 	}
 
 }
