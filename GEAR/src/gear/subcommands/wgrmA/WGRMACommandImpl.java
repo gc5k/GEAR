ba2ae09055eeb49c50b253302a0cabea860059c1
gc5k <chenguobo@gmail.com>
2019-03-25 23:33:05 +0800

    multi-thread for makeGD

--- GEAR/src/gear/subcommands/wgrmA/WGRMACommandImpl.java
+++ GEAR/src/gear/subcommands/wgrmA/WGRMACommandImpl.java
@@ -69,16 +69,19 @@ public class WGRMACommandImpl extends CommandImpl {
 
 		Gcnt = new int[pGM.getNumIndivdial()][pGM.getNumIndivdial()];
 
+		for (int i = 0; i < pGM.getNumIndivdial(); i++) {
+			ArrayList<Integer> mL = NewIt.newArrayList();
+			missList.add(mL);
+		}
+
 		makeGA();
 
 		if (wgrmArgs.isDom()) {
-			makeGD();			
+			if (wgrmArgs.isInbred()) {
+				Logger.printUserLog("Skip dominace because '--inbred' option is switched on.");
+			}
+			makeGD();
 		}
-//		makeAddScore();
-//
-//		if (grmArgs.isDom()) {
-//			makeDomScore();
-//		}
 	}
 
 	private void prepareWeight() {
@@ -124,16 +127,12 @@ public class WGRMACommandImpl extends CommandImpl {
 		long startNanoTime = System.nanoTime();
 		int numSamples = pGM.getNumIndivdial();
 		float[][] gMat = new float[numSamples][pGM.getNumMarker()];
-		for (int i = 0; i < pGM.getNumIndivdial(); i++) {
-			ArrayList<Integer> mL = NewIt.newArrayList();
-			missList.add(mL);
-		}
 
-		Logger.printUserLog("Standardizing genotypes...");
+		Logger.printUserLog("Standardizing genotypes (additive)...");
 		float W = 0;
 		for (int i = 0; i < pGM.getNumMarker(); i++) {
 			if (allelefreq[i][1] == Float.NaN || allelefreq[i][0] == 0 || allelefreq[i][1] == 0 || allelevar[i] == 0) continue;
-			W += weight[i]*weight[i];
+			W += weight[i] * weight[i];
 
 			float f = allelefreq[i][1];
 			float de = 0;
@@ -384,6 +383,268 @@ public class WGRMACommandImpl extends CommandImpl {
 	}
 
 	private void makeGD() {
+		Logger.printUserLog("");
+		Logger.printUserLog("Making dominance genetic relatedness matrix...");
+		long startNanoTime = System.nanoTime();
+		int numSamples = pGM.getNumIndivdial();
+		float[][] dMat = new float[numSamples][pGM.getNumMarker()];
+//		for (int i = 0; i < pGM.getNumIndivdial(); i++) {
+//			ArrayList<Integer> mL = NewIt.newArrayList();
+//			missList.add(mL);
+//		}
+
+		Logger.printUserLog("Standardizing genotypes (dominance)...");
+		float W = 0;
+		for (int i = 0; i < pGM.getNumMarker(); i++) {
+			if (allelefreq[i][1] == Float.NaN || allelefreq[i][0] == 0 || allelefreq[i][1] == 0 || allelevar[i] == 0) continue;
+			W += weight[i]*weight[i]*weight[i]*weight[i];
+
+			float f = allelefreq[i][1];
+			float de = 2 * f * (1-f);
+
+			for (int j = 0; j < pGM.getNumIndivdial(); j++) {
+				int g = pGM.getAdditiveScore(j,i);
+				if (g == Person.MissingGenotypeCode) {
+					dMat[j][i] = 0;
+				} else {
+					if (g == 0) {
+						dMat[j][i] = (0 - 2*f*f) / de * weight[i] * weight[i];
+					} else if (g == 1) {
+						dMat[j][i] = (2*f - 2*f*f) / de * weight[i] * weight[i];
+					} else {
+						dMat[j][i] = ((4*f - 2) -(2*f*f)) / de * weight[i] * weight[i];
+					}
+				}
+			}
+		}
+
+		int grmTriangleSize = (numSamples * (numSamples + 1)) >> 1;
+		if (grmTriangleSize < 15) {
+			Logger.printUserError("Too small sample size. GEAR quit.");
+			System.exit(1);
+		}
+
+		final float[] DA = new float[grmTriangleSize];
+		final float finalWeightSquareSum = W;
+
+		int cpuNum = 1;
+		if (wgrmArgs.isThreadNum()) {
+			cpuNum = wgrmArgs.getThreadNum();
+		}
+
+		final int cpus = cpuNum;
+
+		Logger.printUserLog("Calculating GA with " + cpus + (cpus == 1 ? " thread." : " threads."));
+
+		Thread[] computeThreads = new Thread[cpus];
+		final int[] taskProgresses = new int[cpus];
+		final int smallTaskSize = grmTriangleSize / cpus;
+		final int bigTaskSize = smallTaskSize + 1;
+		final int bigTaskCount = grmTriangleSize % cpus;
+		final int bigTasksEndGrmIndex = bigTaskSize * bigTaskCount;
+
+		for (int i = 0; i < cpus; ++i) {
+			final int threadIndex = i;
+			Thread thread = new Thread() {
+				public void run() {
+					final int taskSize = threadIndex < bigTaskCount ? bigTaskSize : smallTaskSize;
+					final int startGrmIndex = (threadIndex < bigTaskCount) ? bigTaskSize * threadIndex :
+						bigTasksEndGrmIndex + smallTaskSize * (threadIndex - bigTaskCount);
+
+					int sampleIndex1 = (int)Math.sqrt(startGrmIndex << 1);
+					if (((sampleIndex1 * (sampleIndex1 + 1)) >> 1) > startGrmIndex)
+						sampleIndex1 = sampleIndex1 - 1;
+					int sampleIndex2 = startGrmIndex - ((sampleIndex1 * (sampleIndex1 + 1)) >> 1);
+					int taskProgress = 0;
+
+					do {
+						float ws1 = finalWeightSquareSum ;
+						ArrayList<Integer> mL1 = missList.get(sampleIndex1);
+						for(int j = 0; j < mL1.size(); j++) {
+							ws1 -= weight[mL1.get(j)]*weight[mL1.get(j)];		
+						}
+
+						do {
+							float ws2 = ws1;
+							ArrayList<Integer> mL2 = missList.get(sampleIndex2);
+
+							for(int k = 0; k < mL2.size(); k++) {
+								ws2 -= weight[mL2.get(k)]*weight[mL2.get(k)];
+							}
+
+							int mLoci = dMat[sampleIndex1].length;
+							int cnt = 0;
+							if ( mL1.size() <= mL2.size() ) {
+								for (int k = 0; k < mL1.size(); k++) {
+									if (Collections.binarySearch(mL2, mL1.get(k)) >=0) {
+										ws2 += weight[mL1.get(k)]*weight[mL1.get(k)];
+										cnt++;
+									}
+								}
+							} else {
+								for (int k = 0; k < mL2.size(); k++) {
+									if (Collections.binarySearch(mL1, mL2.get(k)) >=0) {
+										ws2 += weight[mL2.get(k)]*weight[mL2.get(k)];
+										cnt++;
+									}
+								}
+							}
+
+							mLoci -= (mL1.size() + mL2.size() - cnt);
+
+							float s = 0; 
+							for (int l = 0; l < dMat[sampleIndex1].length; l++) {
+								s += dMat[sampleIndex1][l] * dMat[sampleIndex2][l];
+							}
+							DA[startGrmIndex + taskProgress] = s/ws2;
+							Gcnt[sampleIndex1][sampleIndex2] = mLoci;
+							taskProgresses[threadIndex] = ++taskProgress;
+						} while (++sampleIndex2 <= sampleIndex1 && taskProgress < taskSize);
+						sampleIndex2 = 0;
+					} while (++sampleIndex1 < numSamples && taskProgress < taskSize);
+				}
+			};
+			thread.start();
+			computeThreads[i] = thread;
+		}
+		
+		Thread progressDisplayThread = new Thread() {
+			public void run() {
+				int totalProgress;
+				do {
+					totalProgress = IntStream.of(taskProgresses).sum();
+					float percentage = Math.min(100f, (float)totalProgress / DA.length * 100f);
+					System.out.print(String.format(
+							"\r[INFO] Calculating dominance genetic relatedness matrix, %.2f%% completed...", percentage));
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {}
+				} while (totalProgress < DA.length);
+				System.out.println();
+			}
+		};
+		progressDisplayThread.start();
+
+		for (int i = 0; i < computeThreads.length; ++i) {
+			try {
+				computeThreads[i].join();
+			} catch (InterruptedException e) {
+				Logger.handleException(e, String.format("Compute thread %d is interrupted.", i));
+			}
+		}
+		try {
+			progressDisplayThread.join();
+		} catch (InterruptedException e) {
+			Logger.printUserError("Progress display thread is interrupted.");
+		}
+		
+		Logger.printUserLog("");
+
+		double grmMean = 0;
+		double grmSq = 0;
+
+		StringBuffer sb = new StringBuffer();
+		sb.append(wgrmArgs.getOutRoot());
+		PrintStream grm = null;
+		BufferedWriter grmGZ = null;
+		if (wgrmArgs.isGZ()) {
+			sb.append(".dom.grm.gz");
+			grmGZ = FileUtil.ZipFileWriter(sb.toString());
+		} else {
+			sb.append(".dom.grm.txt");
+			grm = FileUtil.CreatePrintStream(sb.toString());
+		}
+
+		int cnt = 0;
+		int grmIndex = 0;
+		for (int i = 0; i < Gcnt.length; i++) {
+			for (int j = 0; j <= i; j++, ++grmIndex) {
+				if (i != j) {
+					grmMean += DA[grmIndex];
+					grmSq += DA[grmIndex] * DA[grmIndex];
+					cnt++;
+				}
+				if (wgrmArgs.isGZ()) {
+					try {
+						grmGZ.append((i + 1) + "\t" + (j + 1) + "\t" + Gcnt[i][j] + "\t" + DA[grmIndex] + "\n");
+					} catch (IOException e) {
+						Logger.handleException(e,
+								"error in writing '" + sb.toString() + "' for " + (i + 1) + " " + (j + 1) + ".");
+					}
+				} else {
+					grm.println((i + 1) + "\t" + (j + 1) + "\t" + Gcnt[i][j] + "\t" + DA[grmIndex]);
+				}
+			}
+		}
+
+		if (wgrmArgs.isGZ()) {
+			try {
+				grmGZ.close();
+			} catch (IOException e) {
+				Logger.handleException(e, " error in closing '" + sb.toString() + "'.");
+			}
+		} else {
+			grm.close();
+		}
+		Logger.printUserLog("Writing dominance GRM scores into '" + sb.toString() + "'.");
+		StringBuffer sb_id = new StringBuffer();
+		sb_id.append(wgrmArgs.getOutRoot());
+		PrintStream grm_id = null;
+		sb_id.append(".dom.grm.id");
+		grm_id = FileUtil.CreatePrintStream(sb_id.toString());
+
+		ArrayList<PersonIndex> PI = pGM.getSample();
+		for (int i = 0; i < PI.size(); i++) {
+			grm_id.println(PI.get(i).getFamilyID() + "\t" + PI.get(i).getIndividualID());
+		}
+		grm_id.close();
+		Logger.printUserLog("Writing " + PI.size() + " individuals' information into '" + sb_id.toString() + "'.");
+
+		grmMean /= cnt;
+		grmSq /= cnt;
+		double Effective_sample = -1 / grmMean + 1;
+		double grmSD = (grmSq - grmMean * grmMean) * cnt / (cnt - 1);
+		double Effective_marker = 1 / grmSD;
+
+		DecimalFormat df = new DecimalFormat("0.0000");
+		DecimalFormat dfE = new DecimalFormat("0.00E0");
+		if (Math.abs(grmMean) > 0.0001) {
+			Logger.printUserLog("Mean of dominance genetic relatedness is: " + df.format(grmMean));
+		} else {
+			Logger.printUserLog("Mean of dominance genetic relatedness is: " + dfE.format(grmMean));
+		}
+
+		if (Math.abs(grmSD) > 0.0001) {
+			Logger.printUserLog("Sampling variance of dominance genetic relatedness is: " + df.format(grmSD));
+		} else {
+			Logger.printUserLog("Sampling variance of dominance genetic relatedness is: " + dfE.format(grmSD));
+		}
+
+//		if (Math.abs(Effective_marker) > 0.0001) {
+//			Logger.printUserLog("Effective sample size is: " + df.format(Effective_sample));
+//		} else {
+//			Logger.printUserLog("Effective sample size is: " + dfE.format(Effective_sample));
+//		}
+
+		if (Math.abs(Effective_marker) > 0.0001) {
+			Logger.printUserLog("Effective number of genome segments is: " + df.format(Effective_marker));
+		} else {
+			Logger.printUserLog("Effective number of genome segments is: " + dfE.format(Effective_marker));
+		}
+
+		if(wgrmArgs.isGUI()) {
+			PrintStream gui_file = null;
+			gui_file = FileUtil.CreatePrintStream(wgrmArgs.getOutRoot()+".grm.gui");
+			gui_file.println(df.format(Effective_sample) + "\t" + PI.size());
+			gui_file.println(df.format(Effective_marker) + "\t" + allelefreq.length);
+			gui_file.close();
+		}
+		
+		Logger.printElapsedTime(startNanoTime, "make dominance genetic relatedness matrix");
+	}
+
+	
+	private void makeGD_retired() {
 		float[][] gMat = new float[pGM.getNumIndivdial()][pGM.getNumMarker()];
 
 		for (int i = 0; i < pGM.getNumMarker(); i++) {
