8a7c217887ba3995c9bcbae29004f93e59f24862
gc5k <chenguobo@gmail.com>
2019-03-27 17:53:45 +0800

    recover to original eigengwas for EigenGWASCommandImpl.java.

--- GEAR/src/gear/subcommands/eigengwas/EigenGWASCommandImpl.java
+++ GEAR/src/gear/subcommands/eigengwas/EigenGWASCommandImpl.java
@@ -1,15 +1,11 @@
 package gear.subcommands.eigengwas;
 
+import gear.ConstValues;
 import gear.data.InputDataSet2;
-import gear.family.pedigree.Hukou;
-import gear.family.pedigree.file.BEDReader;
-import gear.family.pedigree.file.MapFile;
+import gear.family.GenoMatrix.GenotypeMatrix;
 import gear.family.pedigree.file.SNP;
-import gear.family.plink.PLINKBinaryParser;
 import gear.family.plink.PLINKParser;
 import gear.qc.sampleqc.SampleFilter;
-import gear.qc.snpqc.SNPFilter;
-import gear.qc.snpqc.SNPFilterPostQC;
 import gear.subcommands.CommandArguments;
 import gear.subcommands.CommandImpl;
 import gear.util.FileUtil;
@@ -17,7 +13,6 @@ import gear.util.Logger;
 import gear.util.NewIt;
 
 import java.io.PrintStream;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -28,10 +23,7 @@ import org.apache.commons.math.stat.regression.SimpleRegression;
 public class EigenGWASCommandImpl extends CommandImpl {
 	private EigenGWASCommandArguments eigenArgs;
 	private SampleFilter sf;
-
-	private MapFile map;
-	private SNPFilter snpFilter;
-	private BEDReader bed;
+	private GenotypeMatrix pGM;
 
 	private int traitIdx;
 	private InputDataSet2 data = new InputDataSet2();
@@ -40,18 +32,12 @@ public class EigenGWASCommandImpl extends CommandImpl {
 	private double lambdaGC = 1;
 	private double[] GC;
 	private double[] pQuantile;
-	private DecimalFormat fmt1 = new DecimalFormat("0.0000");
-	private DecimalFormat fmt2 = new DecimalFormat("0.00E000");
-	private int pheFileIdx = 1;
 
 	public void execute(CommandArguments cmdArgs) {
 
 		eigenArgs = (EigenGWASCommandArguments) cmdArgs;
 
-		PLINKParser pp = PLINKParser.create(eigenArgs);
-		pp.parseSmallFiles();
-		map = pp.getMapData();
-		snpFilter = pp.getSNPFilter();
+		PLINKParser pp = PLINKParser.parse(cmdArgs);
 
 		traitIdx = eigenArgs.getSelectedPhenotype(0);
 		data.addFile(eigenArgs.getFam()); // geno
@@ -61,152 +47,147 @@ public class EigenGWASCommandImpl extends CommandImpl {
 		data.LineUpFiles();
 
 		sf = new SampleFilter(pp.getPedigreeData(), data.getMatchSubjetList());
-		bed = (BEDReader) pp.getPedigreeData();
-		eigenGWASBed();
+		pGM = new GenotypeMatrix(sf.getSample(), pp.getMapData(), cmdArgs);
+
+		eigenGWAS();
 		printResult();
 	}
 
-	private void eigenGWASBed() {
-		ChiSquaredDistributionImpl ci = new ChiSquaredDistributionImpl(1);
-		SNPFilterPostQC snpPostQC = new SNPFilterPostQC(eigenArgs);
-		ArrayList<Hukou> hkBook = bed.getHukouBook();
+	private void eigenGWAS() {
+		// ////TEST
+		// File f = new File(this.eigenArgs.getOutRoot() + ".egwas");
+		// BufferedWriter bw = null;
+		// OutputStreamWriter write = null;
+		// try {
+		// f.createNewFile();
+		// write = new OutputStreamWriter(new FileOutputStream(f));
+		// bw = new BufferedWriter(write);
+		// bw.write("SNP\tCHR\tBP\tRefAllele\tAltAllele\tfreq\tBeta\tSE\tChi\tP\tPGC\tn1\tfreq1\tn2\tfreq2\tFst\n");
+		// } catch (IOException e2) {
+		// e2.printStackTrace();
+		// }
+		// ////TEST
 
-		double threshold = 0.0D;
+		ChiSquaredDistributionImpl ci = new ChiSquaredDistributionImpl(1);
+
+//		int[] gIdx = this.data.getMatchedSubjectIdx(0);
 		int[] pIdx = this.data.getMatchedSubjectIdx(1);
+
 		double[] Y = new double[pIdx.length];
 		ArrayList<Double> pArray = NewIt.newArrayList();
+		double threshold = 0.0D;
 
 		for (int subjectIdx = 0; subjectIdx < Y.length; subjectIdx++) {
-			Y[subjectIdx] = this.data.getVariable(pheFileIdx, pIdx[subjectIdx], this.traitIdx);
+			Y[subjectIdx] = this.data.getVariable(1, pIdx[subjectIdx], this.traitIdx);
 			threshold += Y[subjectIdx];
 		}
 		threshold /= Y.length;
 
-		int numMarkers = map.getMarkerNumberOriginal();
-		int numSamples = bed.getNumIndividuals();
-		int workingSnpIndex = 0;
+		for (int i = 0; i < pGM.getNumMarker(); i++) {
+			SNP snp = pGM.getSNPList().get(i);
 
+			SimpleRegression sReg = new SimpleRegression();
+			double n1 = 0.0D;
+			double n2 = 0.0D;
+			double N = 0.0D;
+			double freq1 = 0.0D;
+			double freq2 = 0.0D;
+			double freq = 0.0D;
 
-		for (int i = 0; i < numMarkers; ++i) {
-
-			if (snpFilter.isSnpIncluded(i)) {
-
-				SimpleRegression sReg = new SimpleRegression();
-				int missingCnt = 0;
-				int sum = 0;
-				int squareSum = 0;
-
-				int[] FmissingCnt = { 0, 0 };
-				int[] Fsum = { 0, 0 };
-				int[] Fn = { 0, 0 };
-
-				for (int j = 0; j < numSamples; j += 4) {
-					int nextByte = bed.readNextByte();
-					int indCnt = j;
-					for (int k = 0; k < 8; k += 2) {
-						if (indCnt == numSamples)
-							break;
-						if (!hkBook.get(indCnt++).isAvailable()) {
-							continue;
-						}
-
-						boolean isMissing = false;
-						int gIdx = Y[indCnt - 1] < threshold ? 0 : 1;
-
-						int genotype = (nextByte >> k) & 0b11;
-
-						switch (genotype) {
-						case PLINKBinaryParser.HOMOZYGOTE_FIRST:
-							break;
-						case PLINKBinaryParser.HETEROZYGOTE:
-							sum += 1;
-							squareSum += 1;
-							Fsum[gIdx] += 1;
-							break;
-						case PLINKBinaryParser.HOMOZYGOTE_SECOND:
-							sum += 2;
-							squareSum += 4;
-							Fsum[gIdx] += 2;
-							break;
-						case PLINKBinaryParser.MISSING_GENOTYPE:
-							++missingCnt;
-							isMissing = true;
-							++FmissingCnt[gIdx];
-							break;
-						}
-						if (!isMissing) {
-							Fn[gIdx]++;
-							sReg.addData(genotype, Y[indCnt - 1]);
-						}
+			for (int j = 0; j < pGM.getNumIndivdial(); j++) {
+				
+//				int g = pGM.getAdditiveScoreOnFirstAllele(gIdx[j], i);
+				int g = pGM.getAdditiveScoreOnFirstAllele(j, i);
+				if (g != ConstValues.MISSING_GENOTYPE) {
+					sReg.addData(g, Y[j]);
+					if (Y[j] < threshold) {
+						n1 += 1.0D;
+						freq1 += g;
+					} else {
+						n2 += 1.0D;
+						freq2 += g;
 					}
+					N += 1.0D;
+					freq += g;
 				}
-				int validSampleCnt = numSamples - missingCnt;
-				double variance = 0;
-				double average = 0;
-				if (validSampleCnt > 2) {
-					average = (double) sum / (2.0*validSampleCnt);
-					variance = (squareSum - validSampleCnt * average * average) / (validSampleCnt - 1);
-				}
-
-				double freq = 1 - average;
-				double maf = freq < 0.5 ? freq : (1 - freq);
-				double freq1 = 1 - (Fsum[0] * 1.0D) / (2.0D * Fn[0]);
-				double freq2 = 1 - (Fsum[1] * 1.0D) / (2.0D * Fn[1]);
-
-				boolean isPassPostQC = snpPostQC.isPassPostQC(maf, missingCnt * 1.0D / numSamples);
-				if (!isPassPostQC) continue;
-
-				double b, b_se;
-				boolean isGood = true;
-				if (freq == 0 || variance == 0) {
-					b = Double.NaN;
-					b_se = Double.NaN;
-					isGood = false;
-				} else {
-					b = sReg.getSlope();
-					b_se = sReg.getSlopeStdErr();
-				}
-				SNP snp = map.getSNP(workingSnpIndex++);
-				EigenGWASResult e1 = new EigenGWASResult(snp, freq, b, b_se, Fn[0], freq1, Fn[1], freq2, isGood);
-				eGWASResult.add(e1);
-				pArray.add(e1.GetP());
 			}
+
+			freq1 /= 2.0D * n1;
+			freq2 /= 2.0D * n2;
+			freq /= 2.0D * N;
+
+			double b, b_se;
+			boolean isGood;
+			if (freq == 0 || freq == 1 || (N < pGM.getNumIndivdial() * eigenArgs.getGENO())
+					|| pGM.getAlleleVar(i) == 0) {
+				b = Double.NaN;
+				b_se = Double.NaN;
+				isGood = false;
+			} else {
+				b = sReg.getSlope();
+				b_se = sReg.getSlopeStdErr();
+				isGood = true;
+			}
+			EigenGWASResult e1 = new EigenGWASResult(snp, freq, b, b_se, n1, freq1, n2, freq2, isGood);
+			eGWASResult.add(e1);
+			pArray.add(e1.GetP());
+
+			// ///TEST
+			// try {
+			// bw.write(e1.printEGWASResult(1) + "\n");
+			// if (i > 10000)
+			// {
+			// bw.flush();
+			// }
+			//
+			// } catch (IOException e) {
+			// e.printStackTrace();
+			// }
+			// ///TEST
 		}
-		snpPostQC.printPostQCSummary();
 		Collections.sort(pArray);
 		int idx = (int) Math.ceil(pArray.size() / 2);
 
-		if (Math.log10(pArray.size()) <= 6) {
+		if(Math.log10(pArray.size()) <= 6) {
 			GC = new double[(int) (Math.log10(pArray.size()))];
 			pQuantile = new double[(int) (Math.log10(pArray.size()))];
 		} else {
 			GC = new double[7];
 			pQuantile = new double[7];
 		}
-		double pMedian = pArray.get(idx);
-		Logger.printUserLog("Median of p values is " + (pMedian > 0.0001? fmt1.format(pMedian):fmt2.format(pMedian))+ " for " + pArray.size() +" analyzed SNPs.");
+		Logger.printUserLog("Median of p values is " + pArray.get(idx));
+
 		try {
 			double chisq = ci.inverseCumulativeProbability(1 - pArray.get(idx).doubleValue());
 			lambdaGC = chisq / 0.4549;
 			GC[0] = lambdaGC;
 			pQuantile[0] = 0.5;
-			for (int i = 1; i < GC.length; i++) {
-				chisq = ci.inverseCumulativeProbability(Math.pow(10, -1 * i));
-				GC[i] = ci.inverseCumulativeProbability(1 - pArray.get((int) (pArray.size() * Math.pow(10, -1 * i))));
-				pQuantile[i] = Math.pow(10, -1 * i);
+			for(int i = 1; i < GC.length; i++) {
+				chisq = ci.inverseCumulativeProbability(Math.pow(10, -1*i));
+				GC[i] = ci.inverseCumulativeProbability(1 - pArray.get((int) (pArray.size()*Math.pow(10, -1*i))) );
+				pQuantile[i] = Math.pow(10, -1*i);
 			}
 		} catch (MathException e) {
 			e.printStackTrace();
 		}
 
-		Logger.printUserLog("Lambda GC is: " + (lambdaGC > 0.0001?fmt1.format(lambdaGC):fmt2.format(lambdaGC)));
+		Logger.printUserLog("Lambda GC is: " + lambdaGC);
 
 		if (eigenArgs.isGUI()) {
 			PrintStream gui_file = null;
-			gui_file = FileUtil.CreatePrintStream(eigenArgs.getOutRoot() + ".egwas.gui");
+			gui_file = FileUtil.CreatePrintStream(eigenArgs.getOutRoot()+".egwas.gui");
 			gui_file.println(lambdaGC);
 			gui_file.close();
 		}
+		// ////TEST
+		// try {
+		// write.close();
+		// bw.close();
+		//
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+		// ////TEST
 	}
 
 	public void printResult() {
@@ -227,11 +208,11 @@ public class EigenGWASCommandImpl extends CommandImpl {
 				}
 			}
 			if (eigenArgs.isTAB()) {
-				// eGWAS.println(e1.printEGWASTab(lambdaGC));
+//				eGWAS.println(e1.printEGWASTab(lambdaGC));
 				eGWAS.println(e1.printEGWASTab(GC[j]));
 			} else {
-				// eGWAS.println(e1.printEGWASResult(GC[j]));
-				eGWAS.println(e1.printEGWASResult(lambdaGC));
+				eGWAS.println(e1.printEGWASResult(GC[j]));
+//				eGWAS.println(e1.printEGWASResult(lambdaGC));				
 			}
 		}
 		eGWAS.close();
